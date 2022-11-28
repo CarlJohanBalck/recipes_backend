2992debd4584006c4231254ad98c729c37e6acaf
Carl B <carljohanbalck@gmail.com>
2022-11-28 10:48:11 +0100

    Added config and updated db queries

--- go_rewrite/recipe.go
+++ go_rewrite/recipe.go
@@ -5,11 +5,23 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tkanos/gonfig"
 )
 
 type Recipe struct {
-	ID         int    `json:"id"`
-	Recipename string `json:"recipename"`
+	recipe_id        int
+	recipe_name      string
+	recipe_url       string
+	recipe_helg      bool
+	recipe_price     int
+	recipe_image_url string
+
+	DB_USERNAME      string
+	DB_PASSWORD      string
+	DB_PORT          string
+	DB_HOST          string
+	DB_NAME          string
+	DB_QUERY_GET_ALL string
 }
 
 func main() {
@@ -18,25 +30,36 @@ func main() {
 
 }
 
+func GetConfig(params ...string) Recipe {
+	conf := Recipe{}
+	env := "dev"
+	if len(params) > 0 {
+		env = params[0]
+	}
+	fileName := fmt.Sprintf("./%s_config.json", env)
+	gonfig.GetConf(fileName, &conf)
+	return conf
+}
+
 func getRecipes() []string {
+	conf := GetConfig()
 	// Create the database handle, confirm driver is present
-	db, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/cool_db?parseTime=true")
+	db, _ := sql.Open("mysql", conf.DB_USERNAME+":"+conf.DB_PASSWORD+"@tcp("+conf.DB_HOST+":"+conf.DB_PORT+")/"+conf.DB_NAME+"?parseTime=true")
 	defer db.Close()
 	s := make([]string, 3)
 
 	// Connect and check the server version
-	results, err := db.Query("SELECT id, name FROM recipe")
+	results, err := db.Query(conf.DB_QUERY_GET_ALL)
 	if err != nil {
 		panic(err.Error())
 	}
 	for results.Next() {
 		var recipe Recipe
-		err = results.Scan(&recipe.ID, &recipe.Recipename)
-		s = append(s, recipe.Recipename)
+		err = results.Scan(&recipe.recipe_name)
+		s = append(s, recipe.recipe_name)
 		if err != nil {
 			panic(err.Error())
 		}
-		/* fmt.Println(recipe.Recipename) */
 	}
 	return s
 
