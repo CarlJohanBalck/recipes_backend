1f345033db77dbbb215cba10231cffa58de611a3
Carl B <carljohanbalck@gmail.com>
2022-11-28 14:13:38 +0100

    Ingredient list

--- go_rewrite/recipe.go
+++ go_rewrite/recipe.go
@@ -9,23 +9,33 @@ import (
 )
 
 type Recipe struct {
-	recipe_id        int
-	recipe_name      string
-	recipe_url       string
-	recipe_helg      bool
-	recipe_price     int
-	recipe_image_url string
+	recipe_id         int
+	recipe_name       string
+	recipe_url        string
+	recipe_helg       bool
+	recipe_price      int
+	recipe_image_url  string
+	ingredient_id     int
+	ingredient_name   string
+	ingredient_amount string
+	ingredient_unit   string
 
-	DB_USERNAME      string
-	DB_PASSWORD      string
-	DB_PORT          string
-	DB_HOST          string
-	DB_NAME          string
-	DB_QUERY_GET_ALL string
+	DB_USERNAME                                 string
+	DB_PASSWORD                                 string
+	DB_PORT                                     string
+	DB_HOST                                     string
+	DB_NAME                                     string
+	DB_QUERY_GET_ALL                            string
+	DB_QUERY_GET_DISH_LIST                      string
+	DB_QUERY_INGREDIENTS_1                      string
+	DB_QUERY_INGREDIENTS_2                      string
+	DB_QUERY_GET_RECIPES_BASED_ON_INGREDIENTS_1 string
+	DB_QUERY_GET_RECIPES_BASED_ON_INGREDIENTS_2 string
+	DB_QUERY_GET_RECIPES_BASED_ON_INGREDIENTS_3 string
 }
 
 func main() {
-	result := getRecipes()
+	result := ingredients_for_recipe()
 	fmt.Println("RESULT: ", result)
 }
 
@@ -63,26 +73,36 @@ func getRecipes() []string {
 	return s
 
 }
-func recipes_for_ingredinets() []string {
+
+func connectDb() *sql.DB {
 	conf := GetConfig()
 	// Create the database handle, confirm driver is present
 	db, _ := sql.Open("mysql", conf.DB_USERNAME+":"+conf.DB_PASSWORD+"@tcp("+conf.DB_HOST+":"+conf.DB_PORT+")/"+conf.DB_NAME+"?parseTime=true")
-	defer db.Close()
-	s := make([]string, 3)
+	return db
+}
 
-	// Connect to maria db and get all recipes names
-	results, err := db.Query(conf.DB_QUERY_GET_ALL)
+func ingredients_for_recipe() []string {
+	conf := GetConfig()
+	// Create the database handle, confirm driver is present
+
+	recipeList := "(12, 13)"
+	s := make([]string, 3)
+	db := connectDb()
+	/* db, _ := sql.Open("mysql", conf.DB_USERNAME+":"+conf.DB_PASSWORD+"@tcp("+conf.DB_HOST+":"+conf.DB_PORT+")/"+conf.DB_NAME+"?parseTime=true") */
+	defer db.Close()
+	statement := conf.DB_QUERY_INGREDIENTS_1 + " " + recipeList + " " + conf.DB_QUERY_INGREDIENTS_2
+	results, err := db.Query(statement)
 	if err != nil {
 		panic(err.Error())
 	}
 	for results.Next() {
 		var recipe Recipe
-		err = results.Scan(&recipe.recipe_name)
-		s = append(s, recipe.recipe_name)
+		err = results.Scan(&recipe.ingredient_amount, &recipe.ingredient_unit, &recipe.ingredient_name)
+		s = append(s, recipe.ingredient_amount, recipe.ingredient_unit, recipe.ingredient_name)
 		if err != nil {
 			panic(err.Error())
 		}
+
 	}
 	return s
-
 }
