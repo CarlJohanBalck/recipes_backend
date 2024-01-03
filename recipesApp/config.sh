a69bcba0f63c78214df61a710333a689f016c694
Carl Johan Balck <carljohanbalck@carls-macbook.local>
2024-01-03 09:16:42 +0100

    trigger updates

--- recipesApp/config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DOCKER_IMAGE_NAME="recipes_backend_amd64"
-DOCKER_IMAGE_NAME_ARM="recipes_backend_arm"
-DOCKER_IMAGE_TAG="latest"
-DOCKER_HUB_USERNAME="carl0222"
-DOCKER_HUB_PASSWORD="Dockerarfanalltidbra921"
